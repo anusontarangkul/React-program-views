1014dee0252391c89ae5518946fd97c525e3f724
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-08 23:34:34 -0700

    add data hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import BarChart from "./components/BarChart"
 import './App.css';
+import { csv } from 'd3';
+import datacsv from './data.csv';
 
 function App() {
+  const [data, setData] = useState()
+  useEffect(() => {
+    csv(datacsv).then(data => {
+      setData(data);
+
+    });
+  }, []);
+  console.log(data)
   return (
     <div className="App">
       <BarChart />
+
     </div>
   );
 }
