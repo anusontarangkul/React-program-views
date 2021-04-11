96d1c2f72811721ba28b8a03d9b58cb109eee5f6
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-10 17:36:43 -0700

    update label

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -10,7 +10,7 @@ const BarChart = ({ label, dataY }) => {
                 data={{
                     labels: label,
                     datasets: [{
-                        label: '# of Votes',
+                        label: 'Total Views',
                         data: dataY,
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
